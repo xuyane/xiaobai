db76dcab757ebdd6d2113f75d85b8a3099b91686
xiaobai <419456942@qq.com>
2018-12-19 08:35:42 +0800

    音乐

---
+++ vue-music/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
+export default {
+  components:{
+    MHeader,
+    Tab
+  }
+}
+</script>
+
+
+<style lang="stylus" scoped rel="stylesheet/stylus">
+    @import "~common/stylus/variable"
+    
+
+    #app 
+      color:$color-theme
+</style>
+
---
+++ vue-music/src/components/search/search.vue
@@ -0,0 +1,11 @@
+ <template>
+   <div>
+     搜索页面
+   </div>
+ </template>
+ <script type="text/ecmascript-6"> </script>
+<style lang="stylus" scoped>
+
+</style>
+
+ 
