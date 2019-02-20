47932f44b6aeae19a4b6408c4e4030b840f73cce
xiaobai <419456942@qq.com>
2019-02-20 17:48:49 +0800

    测试

---
+++ vue-music/src/api/singer.js
@@ -0,0 +1,20 @@
+import jsonp from 'common/js/jsonp'
+import { commonParams, options } from './config'
+
+export function getSingerList() {
+  //const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg?g_tk=1928093487&inCharset=utf-8&outCharset=utf-8&notice=0&format=jsonp&channel=singer&page=list&key=all_all_all&pagesize=100&pagenum=1&hostUin=0&needNewCode=0&platform=yqq&jsonpCallback=jp0'
+  const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg';
+  const data = Object.assign({}, commonParams, {
+    channel: 'singer',
+    page: 'list',
+    key: 'all_all_all',
+    pagesize: 100,
+    pagenum: 1,
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq'
+  })
+  return jsonp(url, data, options)
+}
+
+
--- vue-music/src/base/scroll/scroll.vue
+++ vue-music/src/base/scroll/scroll.vue
@@ -19,6 +19,10 @@ export default {
     data: {
       type: Array,
       default: null
+    },
+    listenScroll: {
+      type: Boolean,
+      default: false
     }
   },
   mounted() {
@@ -35,6 +39,13 @@ export default {
         probeType: this.probeType,
         click: this.click
       });
+
+      if (this.listenScroll) {
+        let me = this;
+        this.scroll.on("scroll", pos => {
+          me.$emit('scroll',pos)
+        });
+      }
     },
     enable() {
       this.scroll && this.scroll.enable();
@@ -44,6 +55,12 @@ export default {
     },
     refresh() {
       this.scroll && this.scroll.refresh();
+    },
+    scrollTo() {
+      this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments);
+    },
+    scrollToElement() {
+      this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments);
     }
   },
   watch: {
